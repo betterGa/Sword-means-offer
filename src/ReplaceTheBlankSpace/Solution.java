117cb3fa285c0d46c5662d50cbd82bca432c5469
yourjia <2086543608@qq.com>
2019-11-20 20:29:55 +0800

    20191120 替换空格

---
+++ src/ReplaceTheBlankSpace/Solution.java
@@ -0,0 +1,19 @@
+package ReplaceTheBlankSpace;
+
+//题目：替换空格
+// 请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+// 例如，当字符串为We Are Happy.
+// 则经过替换之后的字符串为We%20Are%20Happy。
+public class Solution {
+    public String replaceSpace(StringBuffer str) {
+for(int i=0;i<str.length();i++) {
+    if (str.charAt(0)==' ')
+    {
+        str.replace(i,i+1,"%20");
+    }
+}
+return str.toString();
+
+
+    }
+}
