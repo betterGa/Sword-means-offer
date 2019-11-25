3f4a7d7a29721bbd8e18d805ed94d94b634734af
yourjia <2086543608@qq.com>
2019-11-25 14:33:28 +0800

    20191125 两个栈实现队列

--- src/StackToQueue/Solution.java
+++ src/StackToQueue/Solution.java
@@ -4,8 +4,8 @@ import java.util.Stack;
 //用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
 
 //思路：实现队列的push操作，向任意一个栈push即可。
-//如果两个队都为空，就向队A里push
-//之后一直向非空的队A中push
+//如果两个栈都为空，就向栈A里push
+//之后一直向非空的栈A中push
 
 public class Solution {
     Stack<Integer> stack1 = new Stack<Integer>();
@@ -17,13 +17,13 @@ else stack2.push(node);
     }
 
     public int pop() {
-//需要将队A只保留一个元素，其余都转移进队B中
+//需要将栈A只保留一个元素，其余都转移进栈B中
         while (stack1.size()>1)
         {stack2.push(stack1.pop()); }
 
-        //让队A仅剩的这个元素出栈
+        //让栈A仅剩的这个元素出栈
        int peek=stack1.pop();
-        //再把队B中的元素再倒回队A中，A顺序恢复
+        //再把栈B中的元素再倒回栈A中，A顺序恢复
         while (!stack2.isEmpty())
         {stack1.push(stack2.pop());}
         return peek;
