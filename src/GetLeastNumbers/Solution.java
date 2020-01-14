bacdf8fae949b312294a0cbc45d5038df6888c2c
yourjia <2086543608@qq.com>
2020-01-14 12:31:45 +0800

    20200114 最小的K个数

--- src/GetLeastNumbers/Solution.java
+++ src/GetLeastNumbers/Solution.java
@@ -1,4 +1,21 @@
 package GetLeastNumbers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//输入n个整数，找出其中最小的K个数。
+// 例如输入4,5,1,6,2,7,3,8这8个数字，
+// 则最小的4个数字是1,2,3,4,。
+import java.util.*;
 public class Solution {
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        ArrayList result=new ArrayList();
+        if(k>input.length) return result;
+        Arrays.sort(input);
+
+        for(int i=0;i<k;i++)
+            result.add(input[i]);
+        return result;
+
+    }
 }
