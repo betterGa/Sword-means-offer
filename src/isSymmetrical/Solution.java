9cfac9f6fce234bc280ecd782fddc4a1c8fd495a
yourjia <2086543608@qq.com>
2020-02-01 16:03:59 +0800

    2020 0201 对称的二叉树

--- src/isSymmetrical/Solution.java
+++ src/isSymmetrical/Solution.java
@@ -1,4 +1,28 @@
 package isSymmetrical;
 
+ class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+ }
+
+ //请实现一个函数，用来判断一颗二叉树是不是对称的。
+ // 注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
 public class Solution {
-}
+    boolean isSymmetrical(TreeNode pRoot)
+    {
+if(pRoot==null) return true;
+return isSymmetrical(pRoot.left,pRoot.right);
+    }
+     boolean isSymmetrical(TreeNode pLeft,TreeNode pRight)
+     {
+         if(pLeft==null&&pRight==null) return true;
+         if(pLeft==null||pRight==null) return false;
+         if(pLeft.val==pRight.val) return isSymmetrical(pLeft.left,pRight.right)&&isSymmetrical(pLeft.right,pRight.left);
+    else return false;
+     }
+ }
