14ad4cf1af0fde1cf35623789b2eca619823ffeb
yourjia <2086543608@qq.com>
2020-02-07 15:51:53 +0800

    20200207 字符流中第一个不重复的字符

--- src/FirstNonred/Solution.java
+++ src/FirstNonred/Solution.java
@@ -1,4 +1,24 @@
 package FirstNonred;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+//思路：利用Set不允许重复
+//每次插入char就入Set中，遇到重复的，需要从Set中删除该元素，这样Set中放置的都是不重复的元素
+//第一个不重复的就是Set的第一个元素
 public class Solution {
-}
+    //Insert one char from stringstream
+    Set<Character> set=new LinkedHashSet<>();
+    public void Insert(char ch)
+    {
+if(set.add(ch)==false) set.remove(ch);
+    }
+    //return the first appearence once char in current stringstream
+    public char FirstAppearingOnce()
+    {
+if(set.isEmpty()) return '#';
+        else
+{Object[] result=set.toArray();
+    return (char)result[0];
+    }
+}}
--- src/FirstNonred/Test.java
+++ src/FirstNonred/Test.java
@@ -1,4 +1,18 @@
 package FirstNonred;
 
+import java.util.*;
+
 public class Test {
+    @org.junit.Test
+    public void test1()
+    {
+        Set<Character> set=new LinkedHashSet<>();
+        set.add('a');
+        set.add('z');
+        set.add('c');
+        set.add('y');
+        System.out.println(set);}
+
+
+
 }
