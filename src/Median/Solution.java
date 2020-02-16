b5bb2a1ad95b363c9915116530ba8c17cc3b72e8
yourjia <2086543608@qq.com>
2020-02-16 10:55:34 +0800

    20200216 数据流中的中位数

--- src/Median/Solution.java
+++ src/Median/Solution.java
@@ -1,4 +1,34 @@
 package Median;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+//如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，
+// 那么中位数就是所有数值排序之后位于中间的数值。
+// 如果从数据流中读出偶数个数值，
+// 那么中位数就是所有数值排序之后中间两个数的平均值。
+        //我们使用Insert()方法读取数据流，使用GetMedian()方法获取当前读取数据的中位数。
 public class Solution {
+ArrayList<Integer> arrayList=new ArrayList<>();
+    public void Insert(Integer num) {
+        arrayList.add(num);
+
+    }
+
+    public Double GetMedian() {
+
+        arrayList.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1-o2;
+            }
+        });
+int size=arrayList.size();
+        if(size%2==1) return (double)arrayList.get(size/2);
+        else return ((double)arrayList.get(size/2-1)+(double)arrayList.get(size/2))/2;
+    }
+
+
 }
--- src/Median/SolutionTest.java
+++ src/Median/SolutionTest.java
@@ -1,19 +1,37 @@
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+package Median;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 
 import static org.junit.Assert.*;
 
-@RunWith(Arquillian.class)
+
 public class SolutionTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(Median.Solution.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+
+
+    @Test
+    public void insert() {
+        ArrayList<Integer> arrayList=new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+        arrayList.add(6);
+
+        arrayList.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+        System.out.println(arrayList);
     }
 
+    @Test
+    public void getMedian() {
+    }
 }
